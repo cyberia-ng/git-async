7a3b44ceb58495a9572a95495f93b9f2f544051d
cyberia <cyberia@donk.systems>
2026-05-02 15:23:40 +0100

    write some crate-level documentation

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # git-future
 
-An async-first library for reading git repositories
+An async-first Rust library for reading git repositories
 
 ## Usage
+
+The main entry point is the [`Repo`] object, which represents a git
+repository. Refs and objects are looked up via methods on [`Repo`].
 
 The library is agnostic as to the async runtime in use, so consumers must
 implement a couple of traits that provide filesystem operations. See the
@@ -16,18 +19,28 @@ operations.
 A future goal is to provide some standard implementations for commonly-used
 async runtimes.
 
-The main entry point is the [`Repo`] object, which represents a git
-repository. Refs and objects are looked up via methods on [`Repo`].
-
 ## Example
 ```rust
-let foo: u8 = 0;
+async fn example() -> GResult<()> {
+    let repo = Repo::<MyFS>::new(MyDirectory::new("a-repository/.git")).await?;
+    let head = repo.head().await?;
+    let commit = head.peel_to_commit(&repo).await?.unwrap();
+    let message = commit.message();
+    println!("{}", str::from_utf8(message).unwrap());
+    Ok(())
+}
 ```
 
 ## Caveats
+There are a few things this crate cannot (yet) do. They are in scope, so
+future versions may support them, but for now they are not implemented.
 
-- Read only
-- Diff is slow
+- It only supports read operations on git repositories
+- It ignores the working tree. Only operations involving the actual
+  repository structure are supported
+- The diff algorithm is naive and quite slow. I have not looked into how
+  `git diff` manages to be so fast, but I imagine it uses the packfile delta
+  encoding somehow to optimize diffing.
 
 [`file_system`]: https://docs.rs/git-future/latest/git_future/file_system/index.html
 [`Repo`]: https://docs.rs/git-future/latest/git_future/struct.Repo.html
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-//! An async-first library for reading git repositories
+//! An async-first Rust library for reading git repositories
 //!
 //! # Usage
+//!
+//! The main entry point is the [`Repo`] object, which represents a git
+//! repository. Refs and objects are looked up via methods on [`Repo`].
 //!
 //! The library is agnostic as to the async runtime in use, so consumers must
 //! implement a couple of traits that provide filesystem operations. See the
@@ -14,18 +17,49 @@
 //! A future goal is to provide some standard implementations for commonly-used
 //! async runtimes.
 //!
-//! The main entry point is the [`Repo`] object, which represents a git
-//! repository. Refs and objects are looked up via methods on [`Repo`].
-//!
 //! # Example
 //! ```
-//! let foo: u8 = 0;
+//! # use git_future::Repo;
+//! # use git_future::error::GResult;
+//! # use git_future::file_system::{File, Directory, FileSystem, FileSystemError, Offset, DirEntry};
+//! # struct MyFile;
+//! # impl File for MyFile {
+//! #     async fn read_all(&mut self) -> Result<Vec<u8>, FileSystemError> { unimplemented!() }
+//! #     async fn read_segment(&mut self, _: Offset, _: &mut [u8]) -> Result<usize, FileSystemError> { unimplemented! () }
+//! # }
+//! # struct MyDirectory;
+//! # impl MyDirectory { fn new(s: &str) -> Self { unimplemented!() } }
+//! # impl Clone for MyDirectory { fn clone(&self) -> Self { unimplemented!() } }
+//! # impl Directory<MyFile> for MyDirectory {
+//! #     async fn open_subdir(&self, _: &[u8]) -> Result<Self, FileSystemError> { unimplemented!() }
+//! #     async fn list_dir(&self) -> Result<Vec<DirEntry>, FileSystemError> { unimplemented!() }
+//! #     async fn open_file(&self, _: &[u8]) -> Result<MyFile, FileSystemError> { unimplemented!() }
+//! # }
+//! # struct MyFS;
+//! # impl FileSystem for MyFS {
+//! #     type Directory = MyDirectory;
+//! #     type File = MyFile;
+//! # }
+//! async fn example() -> GResult<()> {
+//!     let repo = Repo::<MyFS>::new(MyDirectory::new("a-repository/.git")).await?;
+//!     let head = repo.head().await?;
+//!     let commit = head.peel_to_commit(&repo).await?.unwrap();
+//!     let message = commit.message();
+//!     println!("{}", str::from_utf8(message).unwrap());
+//!     Ok(())
+//! }
 //! ```
 //!
 //! # Caveats
+//! There are a few things this crate cannot (yet) do. They are in scope, so
+//! future versions may support them, but for now they are not implemented.
 //!
-//! - Read only
-//! - Diff is slow
+//! - It only supports read operations on git repositories
+//! - It ignores the working tree. Only operations involving the actual
+//!   repository structure are supported
+//! - The diff algorithm is naive and quite slow. I have not looked into how
+//!   `git diff` manages to be so fast, but I imagine it uses the packfile delta
+//!   encoding somehow to optimize diffing.
 
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(doc, warn(missing_docs))]
